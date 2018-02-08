442d425131be27595d3a0f365e0b746ad4c72f7a
Marko Sustarsic <MarkoS@canneslions.com>
2018-02-08 16:17:16 +0000

    Queries

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
+const Query = require('./resolvers/Query');
+
+const resolvers = {
+  Query,
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/public-roguefox-898/hackernews-node/dev', // the endpoint of the Prisma DB service
+      secret: 'mysecret123', // specified in database/prisma.yml
+      debug: true, // log all GraphQL queryies & mutations
+    }),
+  }),
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
