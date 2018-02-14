376816f7a9720e1622567eba7401fe3c6c6ca2fc
Marko Sustarsic <MarkoS@canneslions.com>
2018-02-14 17:02:39 +0000

    Authentication

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,11 +1,24 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { APP_SECRET } = require('../utils');
+const { APP_SECRET, getUserId } = require('../utils');
 
 function post(parent, args, context, info) {
   const { url, description } = args;
-
-  return context.db.mutation.createLink({ data: { url, description }}, info);
+  const userId = getUserId(context);
+  return context.db.mutation.createLink(
+    {
+      data: {
+        url,
+        description,
+        postedBy: {
+          connect: {
+            id: userId
+          }
+        }
+      }
+    },
+    info,
+  );
 };
 
 async function signup(parent, args, context, info) {
@@ -42,8 +55,32 @@ async function login(parent, args, context, info) {
   };
 };
 
+async function vote(parent, args, context, info) {
+  const userId = getUserId(context);
+  const { linkId } = args;
+
+  const voteExists = await context.db.exists.Vote({
+    user: { id: userId },
+    link: { id: linkId },
+  });
+  if (voteExists) {
+    throw new Error(`Already voted for link: ${linkId}`)
+  };
+
+  return context.db.mutation.createVote(
+    {
+      data: {
+        user: { connect: { id: userId }},
+        link: { connect: { id: linkId }},
+      }
+    },
+    info,
+  );
+}
+
 module.exports = {
   post,
   signup,
   login,
+  vote,
 };
